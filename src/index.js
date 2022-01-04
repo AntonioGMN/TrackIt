127d4609765e9d4518d2e92252684be60c5eab4d
Antonio <antonio.garciamn@gmail.com>
2022-01-04 19:27:40 -0300

    Criandos Arquivos base e styles

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import App from "./app";
+import ReactDOM from "react-dom";
+
+import "./styles/reset.css";
+import "./styles/index.css";
+
+ReactDOM.render(<App />, document.querySelector(".root"));
